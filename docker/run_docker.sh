26beabd39f9f59e8280eb6ca8acae82e0e0882bb
Chris Lee <sihrc.c.lee@gmail.com>
2019-01-17 22:14:58 -0500

    Delete run_docker.sh

--- docker/run_docker.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-docker build -t {{package}} .
-docker stop {{package}}
-docker rm {{package}}
-docker run -v $(pwd):/{{package}} --name {[package}} -dt {{package}} bash
-docker exec -it {{package}} bash
